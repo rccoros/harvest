a073438ff93ad6981c1b1134553375aa6f18e9df
joemel-amparo <joemel.amparo@gmail.com>
2016-10-23 13:06:21 +0800

    Modified calendar.

--- application/views/repeating/top-bar.php
+++ application/views/repeating/top-bar.php
@@ -3,13 +3,15 @@
         <div class="banner" style="background-color: #fee236" style="border: none">
             <a href="<?php echo base_url(); ?>?ap=dashboard">
 				<img src="<?php echo base_url(); ?>img/banner-puti.png" alt="Rapid HTML"/>
+			</a>
         </div>
             <div class="container-fluid" style="right: true">
             <ul class="nav nav-pills main-menu navbar-right">
                 <li if($GET['page']="dashboard"){style="class=\active\";"}><a href="?ap=dashboard">Home</a></li>
                 <li if($GET['page']="calendar"){style="class=\active\";"}><a href="?ap=calendar">Lesson Plan</a></li>
-                <li if($GET['page']="home"){style="class=\active\";"}><a href="?page=home">Statistics</a></li>
-                <li if($GET['page']="home"){style="class=\active\";"}><a href="?ap=classrecord">Class Record</a></li>
+                <li if($GET['page']="statistics"){style="class=\active\";"}><a href="?ap=statistics">Statistics</a></li>
+                <li if($GET['page']="classrecord"){style="class=\active\";"}><a href="?ap=classrecord">Class Record</a></li>
+                <li if($GET['page']="about"){style="class=\active\";"}><a href="?ap=about">About</a></li>
                 <li if($GET['page']="signout")><a href="<?php echo base_url(); ?>?ap=signout">Logout</a></li>
             </ul>
         </div>
