c55acb0ec2ec49634ac8e7577698c69b60918356
joemel-amparo <joemel.amparo@gmail.com>
2016-10-23 12:05:10 +0800

    Modified topbar and css. Added logout function.

--- application/views/repeating/top-bar.php
+++ application/views/repeating/top-bar.php
@@ -1,24 +1,18 @@
 <body>
     <!-- topbar starts -->
-    <div class="navbar navbar-default" role="navigation">
         <div class="banner" style="background-color: #fee236" style="border: none">
             <a href="<?php echo base_url(); ?>?ap=dashboard">
 				<img src="<?php echo base_url(); ?>img/banner-puti.png" alt="Rapid HTML"/>
 			</a>
         </div>
-    </div>
-    
-    <div class="navbar navbar-inverse">
-        <div class="container-fluid">
-            <div class="navbar header">
-                <a class="navbar-brand" href="#">Hope Planner</a>"
-            </div>
-            <ul class="nav nav-pills nav-stacked main-menu">
-                <li href="#">Test</a></li>
-                <li href="#">Test2</a></li>
-                <li href="#">Test3</a></li>
+            <div class="container-fluid" style="right: true">
+            <ul class="nav nav-pills main-menu navbar-right">
+                <li if($GET['page']="dashboard"){style="class=\active\";"}><a href="?ap=dashboard">Home</a></li>
+                <li if($GET['page']="calendar"){style="class=\active\";"}><a href="?ap=calendar">Lesson Plan</a></li>
+                <li if($GET['page']="home"){style="class=\active\";"}><a href="?page=home">Statistics</a></li>
+                <li if($GET['page']="home"){style="class=\active\";"}><a href="?page=home">Class Record</a></li>
+                <li if($GET['page']="signout")><a href="<?php echo base_url(); ?>?ap=signout">Logout</a></li>
             </ul>
         </div>
-	</div>
 </body>
     <!-- topbar ends -->
